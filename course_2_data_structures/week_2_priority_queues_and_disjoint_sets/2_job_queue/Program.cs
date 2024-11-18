0dd4453819a1bb40a521647bcb710dd85a3c66ce
Hoang Minh <minhhoccode111@gmail.com>
2024-11-18 22:20:17 +0700

    add: job queue

--- course_2_data_structures/week_2_priority_queues_and_disjoint_sets/2_job_queue/Program.cs
+++ course_2_data_structures/week_2_priority_queues_and_disjoint_sets/2_job_queue/Program.cs
@@ -1,2 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// starter file convert from Java to C#
+using System;
+using System.Diagnostics;
+using System.Reflection;
+
+class JobQueue
+{
+    static int numWorkers;
+    static int[] jobs;
+
+    static int[] assignedWorker;
+    static long[] startTime;
+
+    static void Main(string[] args)
+    {
+        // if (args != null)
+        // {
+        //     Test();
+        //     return;
+        // }
+
+        Solve();
+    }
+
+    static void ReadData()
+    {
+        string[] input = (Console.ReadLine() ?? throw new Exception("need 'input'")).Split(' ');
+        numWorkers = int.Parse(input[0]);
+        int m = int.Parse(input[1]);
+        jobs = new int[m];
+        for (int i = 0; i < m; ++i)
+        {
+            jobs[i] = int.Parse(Console.ReadLine() ?? throw new Exception("need 'jobs'"));
+        }
+    }
+
+    static void WriteResponse()
+    {
+        for (int i = 0; i < jobs.Length; ++i)
+        {
+            Console.WriteLine($"{assignedWorker[i]} {startTime[i]}");
+        }
+    }
+
+    static void AssignJobs()
+    {
+        // TODO: replace this code with a faster algorithm.
+        assignedWorker = new int[jobs.Length];
+        startTime = new long[jobs.Length];
+        long[] nextFreeTime = new long[numWorkers];
+        for (int i = 0; i < jobs.Length; i++)
+        {
+            int duration = jobs[i];
+            int bestWorker = 0;
+            for (int j = 0; j < numWorkers; ++j)
+            {
+                if (nextFreeTime[j] < nextFreeTime[bestWorker])
+                    bestWorker = j;
+            }
+            assignedWorker[i] = bestWorker;
+            startTime[i] = nextFreeTime[bestWorker];
+            nextFreeTime[bestWorker] += duration;
+        }
+    }
+
+    static void Solve()
+    {
+        ReadData();
+        AssignJobs();
+        WriteResponse();
+    }
+
+    // TODO: add tests
+}
