c11eafaf750c01f0ee139bdbb828da722adc8313
Hoang Minh <minhhoccode111@gmail.com>
2024-11-19 15:17:54 +0700

    add: job queue problem use min-heap priority queue

--- course_2_data_structures/README.md
+++ course_2_data_structures/README.md
@@ -6,3 +6,9 @@ So the file structure is a bit messy.
 Starter files and test files can be found [here](https://www.coursera.org/learn/data-structures/resources/N1p44)
 
 NOTE: We also have to use C# version 7.0
+
+Example how to run input file and expected output file compare to our program output in the `/tests` dir
+
+```bash
+diff <(dotnet run <tests/02) <(cat tests/02.a)
+```
--- course_2_data_structures/week_2_priority_queues_and_disjoint_sets/2_job_queue/Program.cs
+++ course_2_data_structures/week_2_priority_queues_and_disjoint_sets/2_job_queue/Program.cs
@@ -1,74 +1,144 @@
 ﻿// starter file convert from Java to C#
 using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Reflection;
+
+/*
+2 5
+1 2 3 4 5
+---
+0 1
+---
+0 0 | 1
+1 0 | 2
+0 1 | 3
+1 2 | 4
+0 4 | 5
+
+run tests in /tests dir
+diff <(dotnet run <tests/02) <(cat tests/02.a)
+diff <(dotnet run <tests/08) <(cat tests/08.a)
+*/
 
 class JobQueue
 {
-    static int numWorkers;
-    static int[] jobs;
+    static int NumThreads;
+    static int NumJobs;
+    static int[] Jobs;
 
-    static int[] assignedWorker;
-    static long[] startTime;
+    // min-heap priority queue to decide which process will run current job
+    static Thread[] Threads;
 
     static void Main(string[] args)
     {
-        // if (args != null)
-        // {
-        //     Test();
-        //     return;
-        // }
-
-        Solve();
+        ReadData();
+        AssignJobs();
+        // Console.WriteLine("Hello, World!");
     }
 
     static void ReadData()
     {
-        string[] input = (Console.ReadLine() ?? throw new Exception("need 'input'")).Split(' ');
-        numWorkers = int.Parse(input[0]);
-        int m = int.Parse(input[1]);
-        jobs = new int[m];
-        for (int i = 0; i < m; ++i)
-        {
-            jobs[i] = int.Parse(Console.ReadLine() ?? throw new Exception("need 'jobs'"));
-        }
-    }
+        // first line input
+        int[] inputFirstLine = (
+            Console.ReadLine() ?? throw new ArgumentNullException("Need input n")
+        )
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
 
-    static void WriteResponse()
-    {
-        for (int i = 0; i < jobs.Length; ++i)
+        NumThreads = inputFirstLine[0];
+
+        NumJobs = inputFirstLine[1];
+
+        Threads = new Thread[NumThreads];
+
+        // create min-heap priority queue default wait time 0
+        for (int i = 0; i < NumThreads; i++)
         {
-            Console.WriteLine($"{assignedWorker[i]} {startTime[i]}");
+            Threads[i] = new Thread(i, 0);
         }
+
+        // second line input
+        Jobs = (Console.ReadLine() ?? throw new Exception("Need input data"))
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+
+        if (Jobs.Length != NumJobs)
+            throw new Exception("Wrong input length");
     }
 
     static void AssignJobs()
     {
-        // TODO: replace this code with a faster algorithm.
-        assignedWorker = new int[jobs.Length];
-        startTime = new long[jobs.Length];
-        long[] nextFreeTime = new long[numWorkers];
-        for (int i = 0; i < jobs.Length; i++)
+        for (int i = 0; i < NumJobs; i++)
         {
-            int duration = jobs[i];
-            int bestWorker = 0;
-            for (int j = 0; j < numWorkers; ++j)
-            {
-                if (nextFreeTime[j] < nextFreeTime[bestWorker])
-                    bestWorker = j;
-            }
-            assignedWorker[i] = bestWorker;
-            startTime[i] = nextFreeTime[bestWorker];
-            nextFreeTime[bestWorker] += duration;
+            long job = Jobs[i];
+            Console.WriteLine($"{Threads[0].Index} {Threads[0].WaitTime}");
+            Threads[0].WaitTime += job;
+            SiftDown(0);
         }
     }
 
-    static void Solve()
+    static int Parent(int i) => (i - 1) / 2;
+
+    static int Left(int i) => i * 2 + 1;
+
+    static int Right(int i) => i * 2 + 2;
+
+    static void SiftDown(int i)
     {
-        ReadData();
-        AssignJobs();
-        WriteResponse();
+        int l = Left(i);
+        int r = Right(i);
+
+        int min = i;
+
+        int len = Threads.Length;
+
+        if (
+            l < len
+            && (
+                (Threads[l].WaitTime < Threads[min].WaitTime)
+                || (
+                    Threads[l].WaitTime == Threads[min].WaitTime
+                    && Threads[l].Index < Threads[min].Index
+                )
+            )
+        )
+        {
+            min = l;
+        }
+        if (
+            r < len
+            && (
+                (Threads[r].WaitTime < Threads[min].WaitTime)
+                || (
+                    Threads[r].WaitTime == Threads[min].WaitTime
+                    && Threads[r].Index < Threads[min].Index
+                )
+            )
+        )
+        {
+            min = r;
+        }
+        if (i != min)
+        {
+            var tmp = Threads[min];
+            Threads[min] = Threads[i];
+            Threads[i] = tmp;
+            SiftDown(min);
+        }
     }
 
-    // TODO: add tests
+    class Thread
+    {
+        public int Index { get; set; }
+        public long WaitTime { get; set; }
+
+        public Thread(int index, long waitTime = 0)
+        {
+            Index = index;
+            WaitTime = waitTime;
+        }
+    }
 }
