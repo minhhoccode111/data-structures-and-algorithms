7f6d00d10fb82883e415d311d8b657a6fe6bb9dd
Hoang Minh <minhhoccode111@gmail.com>
2024-11-20 22:50:45 +0700

    add: hash tables: phone book problem

---
+++ course_2_data_structures/week_3_hash_tables/1_phone_book/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static int N;
+    static Hashtable PhoneBook = new Hashtable();
+
+    static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello, World!");
+        Solve();
+    }
+
+    static void Solve()
+    {
+        N = int.Parse(Console.ReadLine() ?? throw new Exception("Need input n"));
+        for (int i = 0; i < N; i++)
+        {
+            string[] data = (Console.ReadLine() ?? throw new Exception("Need input data")).Split(
+                ' '
+            );
+            string action = data[0];
+            string number = data[1];
+            if (action == "add")
+            {
+                string name = data[2];
+                PhoneBook[number] = name;
+            }
+            else if (action == "find")
+            {
+                if (PhoneBook.ContainsKey(number))
+                    Console.WriteLine(PhoneBook[number]);
+                else
+                    Console.WriteLine("not found");
+            }
+            else if (action == "del")
+            {
+                PhoneBook.Remove(number);
+            }
+        }
+    }
+}
