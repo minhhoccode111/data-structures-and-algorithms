1b15f0e707dce2c960487d2844b6ce46a0f99698
Hoang Minh <minhhoccode111@gmail.com>
2024-11-26 11:29:17 +0700

    add: graph reachability problem

--- course_3_algorithms_on_graphs/week_1_graph_decomposition1/1_reachability/Program.cs
+++ course_3_algorithms_on_graphs/week_1_graph_decomposition1/1_reachability/Program.cs
@@ -2,47 +2,77 @@
 using System.Collections.Generic;
 using System.Linq;
 
-public class Program
+public class Reachability
 {
-    public static int Reach(List<List<int>> adj, int x, int y)
+    // init a visited array to keep trach of visited nodes
+    static bool[] visited;
+
+    // helper function for DFS
+    static bool DFS(List<int>[] adj, int current, int target)
     {
-        // write your code here
+        // if the current node is the target, return true
+        if (current == target)
+            return true;
+
+        // mark the current node as visited
+        visited[current] = true;
+
+        // explore all neighbors of the current node
+        foreach (int neighbor in adj[current])
+        {
+            // if the neighbor is not visited, recursively call DFS
+            if (!visited[neighbor])
+                // if neighbor is the target, return true
+                if (DFS(adj, neighbor, target))
+                    return true;
+        }
+
+        // if no that is found, return false
+        return false;
+    }
+
+    private static int Reach(List<int>[] adj, int x, int y)
+    {
+        visited = new bool[adj.Length];
+
+        // return 1 or 0 base on function call DFS's result using x and y
+        if (DFS(adj, x, y))
+            return 1;
         return 0;
     }
 
-    public static void Main(string[] args)
+    public static void Main()
     {
-        // get first line
-        string input = Console.ReadLine();
-        int[] data = input.Split(' ').Select(int.Parse).ToArray();
-
-        // first line, first <int> will be 'n', number of vertices
-        int n = data[0];
-        // first line, second <int> will be 'm', number of edges
-        int m = data[1];
-
-        // init a 2-D array with n cells to store list type <int>
-        List<List<int>> adj = new List<List<int>>(n);
-        // loop through each cell in the array
+        // read first line input
+        int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        // n: vertices, m: edges
+        int n = input[0];
+        int m = input[1];
+        // an array of adjacency lists<int>, base on vertices n
+        List<int>[] adj = new List<int>[n];
         for (int i = 0; i < n; i++)
         {
-            // init a new list of <int>
-            adj.Add(new List<int>());
+            adj[i] = new List<int>();
         }
-
-        // loop from 0 to m
-        for (int i = 0; i < m; i++)
+        // loop through m edges
+        for (int j = 0; j < m; j++)
         {
-            //
-            int a = data[2 * i + 2] - 1;
-            int b = data[2 * i + 3] - 1;
-            adj[a].Add(b);
-            adj[b].Add(a);
+            // read input for each edge
+            int[] line = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            // extract x and y, remember to use indexes
+            int lineX = line[0] - 1;
+            int lineY = line[1] - 1;
+            // add to adjacency lists
+            adj[lineX].Add(lineY);
+            adj[lineY].Add(lineX);
         }
 
-        int x = data[2 * m + 2] - 1;
-        int y = data[2 * m + 3] - 1;
+        // last line, to check if x have path to y
+        int[] lastLine = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int x = lastLine[0] - 1;
+        int y = lastLine[1] - 1;
 
+        // print result of reach function with adjacency lists and x and y
         Console.WriteLine(Reach(adj, x, y));
     }
 }
