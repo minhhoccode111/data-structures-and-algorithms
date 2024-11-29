24a1f32fbf44ba905ecb2ad271e44ef4b3d8f1c3
minhhoccode111 <minhhoccode111@gmail.com>
2024-11-29 16:44:36 +0700

    add: cs curriculum

---
+++ course_3_algorithms_on_graphs/week_2_graph_decomposition2/1_cs_curriculum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+
+diff <(dotnet run <test/1) <(cat test/1.a)
+diff <(dotnet run <test/2) <(cat test/2.a)
+
+*/
+
+public class Acyclicity
+{
+    private static int Acyclic(List<int>[] adj)
+    {
+        throw new Exception("Not implemented yet");
+    }
+
+    public static void Main(string[] args)
+    {
+        string[] firstLineInput = Console.ReadLine().Split(' ');
+        int vertices = int.Parse(firstLineInput[0]);
+        int edges = int.Parse(firstLineInput[1]);
+        List<int>[] adjacencyList = new List<int>[vertices];
+
+        for(int i = 0; i < vertices; i++)
+        {
+            adjacencyList[i] = new List<int>();
+        }
+
+        for (int i = 0; i < edges; i++)
+        {
+            string[] currentLineInput = Console.ReadLine().Split(' ');
+            int x = int.Parse(currentLineInput[0]);
+            int y = int.Parse(currentLineInput[1]);
+            adjacencyList[x - 1].Add(y - 1);
+        }
+
+        Console.WriteLine(Acyclic(adjacencyList));
+
+    }
+
+}
--- course_3_algorithms_on_graphs/week_2_graph_decomposition2/notes/notes.md
+++ course_3_algorithms_on_graphs/week_2_graph_decomposition2/notes/notes.md
@@ -3,3 +3,31 @@
 DAG: Directed Acyclic Graph
 source: a vertex with no incoming edges
 sink: a vertex with no outgoing edges
+
+## Finding all nodes reachable from a particular node
+
+```
+explore(G,v)
+input: G = (V,E) is a graph; v in V
+output: visited(u) is set to true for all nodes u reachable from v
+
+visited(v) = true
+previsit(v)
+for each edge (v, u) in E:
+  if not visited(u):
+    explore(G, u)
+postvisit(v)
+```
+
+## Depth-first search
+
+```
+dfs(G)
+
+for all v in V:
+  visited(v) = false
+
+for all v in V:
+  if not visited(v):
+    explore(G,v)
+```
