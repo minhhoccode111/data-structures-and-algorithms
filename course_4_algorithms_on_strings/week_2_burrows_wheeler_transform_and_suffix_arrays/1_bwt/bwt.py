25c99102fa2edee145c61421c645a7155a1453a4
minhhoccode111 <minhhoccode111@gmail.com>
2024-12-11 11:33:02 +0700

    add: course 4: week 2: problem 1: construct the Burrows-Wheeler Transform of a String

--- course_4_algorithms_on_strings/week_2_burrows_wheeler_transform_and_suffix_arrays/1_bwt/bwt.py
+++ course_4_algorithms_on_strings/week_2_burrows_wheeler_transform_and_suffix_arrays/1_bwt/bwt.py
@@ -1,9 +1,28 @@
-# python3
 import sys
 
-def BWT(text):
-    return ""
 
-if __name__ == '__main__':
+"""
+
+diff <(python3 bwt.py < test/1) <(cat test/1.a)
+diff <(python3 bwt.py < test/2) <(cat test/2.a)
+
+"""
+
+
+import sys
+
+
+def BWT(text):
+    # Generate all rotations of the text
+    rotations = [text[i:] + text[:i] for i in range(len(text))]
+    # Sort the rotations lexicographically
+    rotations.sort()
+    # Construct the result by taking the last character of each rotation
+    result = "".join(rotation[-1] for rotation in rotations)
+    return result
+
+
+if __name__ == "__main__":
     text = sys.stdin.readline().strip()
     print(BWT(text))
+
