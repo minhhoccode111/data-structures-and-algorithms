fa0aa18b0be6308f8b9ebb269b42b1e28007f91f
minhhoccode111 <minhhoccode111@gmail.com>
2024-12-11 09:09:11 +0700

    add: course 4: week 1: build a trie problem

--- course_4_algorithms_on_strings/week_1_suffix_trees/1_trie/Program.cs
+++ course_4_algorithms_on_strings/week_1_suffix_trees/1_trie/Program.cs
@@ -8,11 +8,57 @@ diff <(dotnet run < test/3) <(cat test/3.a)
 
 
 using System;
+using System.Collections.Generic;
 
-public class Program
+class Program
 {
-    public static void Main(string[] args)
+    static List<Dictionary<char, int>> BuildTrie(List<string> patterns)
     {
-        Console.WriteLine("Hello, World!");
+        if (patterns.Count == 0)
+            return new List<Dictionary<char, int>>();
+
+        var trie = new List<Dictionary<char, int>> { new Dictionary<char, int>() };
+        int root = 0,
+            count = 0;
+
+        foreach (var pattern in patterns)
+        {
+            int current = root;
+            foreach (char c in pattern)
+            {
+                if (trie[current].ContainsKey(c))
+                {
+                    current = trie[current][c];
+                }
+                else
+                {
+                    trie.Add(new Dictionary<char, int>());
+                    trie[current][c] = ++count;
+                    current = count;
+                }
+            }
+        }
+
+        return trie;
+    }
+
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        List<string> patterns = new List<string>();
+        for (int i = 0; i < n; i++)
+        {
+            string s = Console.ReadLine();
+            patterns.Add(s);
+        }
+
+        var trie = BuildTrie(patterns);
+        for (int i = 0; i < trie.Count; i++)
+        {
+            foreach (var kvp in trie[i])
+            {
+                Console.WriteLine($"{i}->{kvp.Value}:{kvp.Key}");
+            }
+        }
     }
 }
