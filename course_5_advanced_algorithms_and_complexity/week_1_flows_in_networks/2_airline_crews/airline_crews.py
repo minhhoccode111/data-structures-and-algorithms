23f71b0b70da5871d5da7c5f01a383e575a2b488
minhhoccode111 <minhhoccode111@gmail.com>
2024-12-17 13:45:48 +0700

    add: course 5: week 1: problem 2: airline_crews

--- course_5_advanced_algorithms_and_complexity/week_1_flows_in_networks/2_airline_crews/airline_crews.py
+++ course_5_advanced_algorithms_and_complexity/week_1_flows_in_networks/2_airline_crews/airline_crews.py
@@ -1,33 +1,45 @@
 # python3
+
+
 class MaxMatching:
     def read_data(self):
         n, m = map(int, input().split())
-        adj_matrix = [list(map(int, input().split())) for i in range(n)]
-        return adj_matrix
+        adj_matrix = [list(map(int, input().split())) for _ in range(n)]
+        return n, m, adj_matrix
 
     def write_response(self, matching):
         line = [str(-1 if x == -1 else x + 1) for x in matching]
-        print(' '.join(line))
+        print(" ".join(line))
 
-    def find_matching(self, adj_matrix):
-        # Replace this code with an algorithm that finds the maximum
-        # matching correctly in all cases.
-        n = len(adj_matrix)
-        m = len(adj_matrix[0])
+    def find_matching(self, n, m, adj_matrix):
+        # find augmenting path
+        def dfs(flight, visited, match):
+            for crew in range(m):
+                if adj_matrix[flight][crew] and not visited[crew]:
+                    visited[crew] = True
+                    if match[crew] == -1 or dfs(match[crew], visited, match):
+                        match[crew] = flight
+                        return True
+            return False
+
+        # find maximum matching
+        match = [-1] * m  # save current pilot of each crew
+        for flight in range(n):
+            visited = [False] * m
+            dfs(flight, visited, match)
+
         matching = [-1] * n
-        busy_right = [False] * m
-        for i in range(n):
-            for j in range(m):
-                if adj_matrix[i][j] and matching[i] == -1 and (not busy_right[j]):
-                    matching[i] = j
-                    busy_right[j] = True
+        for crew in range(m):
+            if match[crew] != -1:
+                matching[match[crew]] = crew
         return matching
 
     def solve(self):
-        adj_matrix = self.read_data()
-        matching = self.find_matching(adj_matrix)
+        n, m, adj_matrix = self.read_data()
+        matching = self.find_matching(n, m, adj_matrix)
         self.write_response(matching)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     max_matching = MaxMatching()
     max_matching.solve()
