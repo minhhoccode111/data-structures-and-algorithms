c8e85896c03ee13d0063cbd920fce17f9c0c576c
minhhoccode111 <minhhoccode111@gmail.com>
2024-12-18 10:16:56 +0700

    add: course 5: week 3: problem 1: gsm network

--- course_5_advanced_algorithms_and_complexity/week_3_NP_complete_problems/1_gsm_network/gsm_network.py
+++ course_5_advanced_algorithms_and_complexity/week_3_NP_complete_problems/1_gsm_network/gsm_network.py
@@ -1,14 +1,34 @@
 # python3
-n, m = map(int, input().split())
-edges = [ list(map(int, input().split())) for i in range(m) ]
 
-# This solution prints a simple satisfiable formula
-# and passes about half of the tests.
-# Change this function to solve the problem.
+
+import itertools
+
+
+def varnum(i, j):
+    return i + (j - 1) * n
+
+
+def exactly_one_of(literals):
+    clauses.append([l for l in literals])
+    for pair in itertools.combinations(literals, 2):
+        clauses.append([-l for l in pair])
+
+
 def printEquisatisfiableSatFormula():
-    print("3 2")
-    print("1 2 0")
-    print("-1 -2 0")
-    print("1 -2 0")
+    print(n * 4 + m * 3, n * 3)
+    for i in clauses:
+        for j in range(len(i)):
+            print(i[j], end=" ")
+        print(0)
 
+
+n, m = map(int, input().split())
+edges = [list(map(int, input().split())) for i in range(m)]
+clauses = []
+digits = range(1, 4)
+for i in range(n):
+    exactly_one_of([varnum(i + 1, j) for j in digits])
+for j in edges:
+    for r in digits:
+        clauses.append([-varnum(j[0], r), -varnum(j[1], r)])
 printEquisatisfiableSatFormula()
