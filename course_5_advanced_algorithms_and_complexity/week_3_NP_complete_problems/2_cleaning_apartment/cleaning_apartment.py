ec01f5595e35e604e07096b96fc6c8bd1c8959ef
minhhoccode111 <minhhoccode111@gmail.com>
2024-12-18 10:32:14 +0700

    add: course 5: week 3: problem 2: cleaning apartment

--- course_5_advanced_algorithms_and_complexity/week_3_NP_complete_problems/2_cleaning_apartment/cleaning_apartment.py
+++ course_5_advanced_algorithms_and_complexity/week_3_NP_complete_problems/2_cleaning_apartment/cleaning_apartment.py
@@ -1,14 +1,48 @@
 # python3
-n, m = map(int, input().split())
-edges = [ list(map(int, input().split())) for i in range(m) ]
 
-# This solution prints a simple satisfiable formula
-# and passes about half of the tests.
-# Change this function to solve the problem.
+import itertools
+
+n, m = map(int, input().split())
+edges = [list(map(int, input().split())) for i in range(m)]
+adj = [[] for _ in range(n)]
+for a, b in edges:
+    adj[a - 1].append(b)
+    adj[b - 1].append(a)
+
+clauses = []
+digits = range(1, n + 1)
+
+
+def varnum(i, j):
+    return i + (j - 1) * n
+
+
+def exactly_one_of(literals):
+    clauses.append([l for l in literals])
+
+    for pair in itertools.combinations(literals, 2):
+        clauses.append([-l for l in pair])
+
+
+for i in range(n):
+    exactly_one_of([varnum(i + 1, j) for j in digits])
+
+for j in range(n):
+    exactly_one_of([varnum(i, j + 1) for i in digits])
+
+listOfNonAdj = [[] for _ in range(n)]
+
+for i in range(n):
+    for j in range(n - 1):
+        clauses.append([-varnum(j + 1, i + 1)] + [varnum(j + 2, z) for z in adj[i]])
+
+
 def printEquisatisfiableSatFormula():
-    print("3 2")
-    print("1 2 0")
-    print("-1 -2 0")
-    print("1 -2 0")
+    print(len(clauses), n * n)
+    for i in clauses:
+        for j in range(len(i)):
+            print(i[j], end=" ")
+        print(0)
+
 
 printEquisatisfiableSatFormula()
