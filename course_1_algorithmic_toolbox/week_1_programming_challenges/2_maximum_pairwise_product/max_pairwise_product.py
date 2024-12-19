e85609cc7e9dac4bf93f4e9b3849d40e3f055955
minhhoccode111 <minhhoccode111@gmail.com>
2024-12-19 16:35:05 +0700

    refactor: course 1: week 1: max pairwise product

--- course_1_algorithmic_toolbox/week_1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ course_1_algorithmic_toolbox/week_1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -1,29 +1,32 @@
-# # starter solution
-# def max_pairwise_product(numbers):
-#     n = len(numbers)
-#     max_product = 0
-#     for first in range(n):
-#         for second in range(first + 1, n):
-#             max_product = max(max_product, numbers[first] * numbers[second])
-#     return max_product
+# find the maximum product of two distinct numbers in a sequence of non-negative integers
 
 
-def max_pairwise_product(numbers):
-    index1 = 0
-    for i1, val1 in enumerate(numbers):
-        if val1 > numbers[index1]:
-            index1 = i1
+# efficient because only need 1 loop
+# bigO (n)
+def max_pairwise_product(n):
+    # store the two largest numbers
+    max1, max2 = 0, 0
 
-    # in case index1 is 0
-    index2 = 1 if index1 == 0 else 0
-    for i2, val2 in enumerate(numbers):
-        if val2 > numbers[index2] and i2 != index1:
-            index2 = i2
+    # loop through each item in list
+    for num in n:
+        # if item greater than max1
+        if num > max1:
+            # shift the current largest to second largest
+            max2 = max1
+            # update the largest number
+            max1 = num
+        # else item greater than max2
+        elif num > max2:
+            # update the second largest
+            max2 = num
 
-    return numbers[index1] * numbers[index2]
+    return max1 * max2
 
 
 if __name__ == "__main__":
+    # ignore the size of the input
     _ = int(input())
+    # split the string to array of numbers
     input_numbers = list(map(int, input().split()))
+    # print the result
     print(max_pairwise_product(input_numbers))
